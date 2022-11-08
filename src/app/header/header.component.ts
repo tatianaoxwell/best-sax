1701639afed63536c9009d7a82cbdf72d48d29bb
tatianaoxwell <oxwelltatiana@gmail.com>
2022-11-08 16:34:13 +0800

    mixins

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,21 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { IMenueItem } from './menu.model';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  menueItems: IMenueItem[] = [
+	{ segment: 'products', content: "The Best Sax’s" },
+	{ segment: 'cart', content: 'Cart' },
+    { segment: 'about', content: 'About BSS' },
+    { segment: 'contact-us', content: 'Contact Us' },
+  ];
 
-	@Output() toggleSidenav: EventEmitter<void> = new EventEmitter<void>();
-	constructor() { }
-  
-	ngOnInit(): void {
-	}
-  
-  }
-  
+  @Output() toggleSidenav: EventEmitter<void> = new EventEmitter<void>();
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ src/app/header/menu.model.ts
@@ -0,0 +1,4 @@
+export interface IMenueItem {
+	segment: string;
+	content: string;
+}
